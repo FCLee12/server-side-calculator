5c0388812028cec2e7e663c0a878bb28a880def9
Fue Lee <fueclee.12@gmail.com>
2023-03-03 13:00:30 -0600

    added some logic to grab inputs from input fields and started on a couple functions

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,29 @@
+console.log( 'JS ready' );
+
 $(document).ready( onReady );
 
 function onReady() {
-    
+    console.log( 'jQuery ready' );
+
+    // listener for the equals button
+        // should function like a submit button
+    $( '#solve' ).on('click', calculateThis)
+
+    // listener for the operator buttons
+        // remember to use $(this) to grab the text from the button
+    $('.operatorBtn').on('click', useOperator)
+}
+
+function useOperator() {
+    let theOperator = $(this).text();
+    console.log( theOperator );
+}
+
+function calculateThis() {
+    // collecting all inputs from the form
+    let firstInput = $('#input1').val();
+    let secondInput = $('#input2').val();
+
+    console.log( firstInput );
+    console.log( secondInput );
 }
