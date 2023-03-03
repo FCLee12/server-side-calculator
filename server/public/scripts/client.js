f6f9571fe01077214b8e272fdee222ce36493444
Fue Lee <fueclee.12@gmail.com>
2023-03-03 16:17:22 -0600

    server side post and get outline functional

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,28 +2,36 @@ console.log( 'JS ready' );
 
 $(document).ready( onReady );
 
+// variables being sent in POST
+let theOperator = '';
+let firstInput = 0;
+let secondInput =0;
+
 function onReady() {
     console.log( 'jQuery ready' );
 
     // listener for the equals button
         // should function like a submit button
-    $( '#solve' ).on('click', calculateThis)
+    $( '#solve' ).on('click', calculateThese)
 
     // listener for the operator buttons
         // remember to use $(this) to grab the text from the button
     $('.operatorBtn').on('click', useOperator)
 }
 
+// collects the operator text from an operator button press
+    // will be called in the POST to set value in object to be passed in POST data section
 function useOperator() {
-    let theOperator = $(this).text();
-    console.log( theOperator );
+    theOperator = $(this).text();
+    console.log( 'this is theOperator:', theOperator );
 }
 
-function calculateThis() {
+// will be called in the POST to set value in object to be passed in POST data section
+function calculateThese() {
     // collecting all inputs from the form
-    let firstInput = $('#input1').val();
-    let secondInput = $('#input2').val();
+    firstInput = $('#input1').val();
+    secondInput = $('#input2').val();
 
-    console.log( firstInput );
-    console.log( secondInput );
+    console.log( 'this is the firstInput:', firstInput );
+    console.log( 'this is the secondInput:', secondInput );
 }
